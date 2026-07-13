32da0741c72f7c8f3c394dad9d665cf2babc0f91
Ingo Ruddat <42838916+Ruddat@users.noreply.github.com>
2026-07-13 09:06:59 +0200

    Fix sitemap index and add app icons

--- resources/views/sitemaps/categories.blade.php
+++ resources/views/sitemaps/categories.blade.php
@@ -1,9 +1,20 @@
 {!! '<?xml version="1.0" encoding="UTF-8"?>' !!}
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 @foreach ($categories as $category)
+@php
+    $slug = is_string($category)
+        ? $category
+        : $category->slug;
+
+    $lastmod = is_string($category)
+        ? null
+        : ($category->source_updated_at ?? $category->updated_at);
+@endphp
     <url>
-        <loc>{{ route('catalog.categories.show', ['locale' => 'de', 'categorySlug' => $category->slug]) }}</loc>
-        <lastmod>{{ ($category->source_updated_at ?? $category->updated_at)->utc()->toAtomString() }}</lastmod>
+        <loc>{{ route('catalog.categories.show', ['locale' => 'de', 'categorySlug' => $slug]) }}</loc>
+@if ($lastmod)
+        <lastmod>{{ $lastmod->utc()->toAtomString() }}</lastmod>
+@endif
     </url>
 @endforeach
 </urlset>
